dcb4afa62a6f967e02e4a1c6092d0d0a00aca7f5
Hawk <hawk.kim1974@gmail.com>
2026-01-20 23:29:55 +0900

    working on page switching!

--- main_window.c
+++ main_window.c
@@ -3,19 +3,41 @@
 struct _MainWindow {
   AdwApplicationWindow parent_instance;
 
-  GtkButton *sidebar_toggle;
+  GtkButton           *sidebar_toggle;
   AdwOverlaySplitView *split_view;
-  AdwStatusPage *status_page;
+  AdwViewStack        *main_stack;
 };
 
 G_DEFINE_FINAL_TYPE (MainWindow, main_window, ADW_TYPE_APPLICATION_WINDOW)
 
+/* Toggle sidebar visibility */
 static void
 toggle_sidebar (GtkButton *button, MainWindow *self)
 {
   gboolean shown = adw_overlay_split_view_get_show_sidebar (self->split_view);
   adw_overlay_split_view_set_show_sidebar (self->split_view, !shown);
 }
+
+/* Switch to Documents page */
+static void
+show_documents (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+  MainWindow *self = MAIN_WINDOW (user_data);
+  adw_view_stack_set_visible_child_name (self->main_stack, "documents");
+}
+
+/* Switch to Preferences page */
+static void
+show_preferences (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+  MainWindow *self = MAIN_WINDOW (user_data);
+  adw_view_stack_set_visible_child_name (self->main_stack, "preferences");
+}
+
+static const GActionEntry win_actions[] = {
+  { "show-documents",   show_documents   },
+  { "show-preferences", show_preferences },
+};
 
 static void
 main_window_class_init (MainWindowClass *klass)
@@ -27,7 +49,7 @@ main_window_class_init (MainWindowClass *klass)
 
   gtk_widget_class_bind_template_child (widget_class, MainWindow, sidebar_toggle);
   gtk_widget_class_bind_template_child (widget_class, MainWindow, split_view);
-  gtk_widget_class_bind_template_child (widget_class, MainWindow, status_page);
+  gtk_widget_class_bind_template_child (widget_class, MainWindow, main_stack);
 
   gtk_widget_class_bind_template_callback (widget_class, toggle_sidebar);
 }
@@ -36,4 +58,13 @@ static void
 main_window_init (MainWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  /* Register window-specific actions (win.*) */
+  g_action_map_add_action_entries (G_ACTION_MAP (self),
+                                   win_actions,
+                                   G_N_ELEMENTS (win_actions),
+                                   self);
+
+  /* Ensure we start on the documents page */
+  adw_view_stack_set_visible_child_name (self->main_stack, "documents");
 }
